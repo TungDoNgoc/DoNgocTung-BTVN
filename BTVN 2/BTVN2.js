9184607a0f63f846033c1ac637fc30c35bee639d
Tung <tungdongoc98@gmail.com>
2020-10-05 21:34:33 +0700

    initial commit

---
+++ BTVN 2/BTVN2.js
@@ -0,0 +1,18 @@
+// 1)	Boolean là loại dữ liệu logic chỉ có giá trị true hoặc false, thường sử dụng để quyết định đoạn mã nào được thực thi (như trong lệnh if) hay lặp lại (trong vòng lặp for). VD: +if ( 1 == 1 ) {console.log(`helo world`);}
+//                   +if ( a == 1 ) {console.log(`helo world`);}
+//                   else if (a != 1 ) {console.log(`bye world`);}
+//                    +for(a = 0; a > 1; a++) {console.log(`helo world`);}
+// 2)	Flowchart(Lưu đồ thuật toán) là công cụ dùng để biểu diễn thuật toán, mô tả nhập (input), dữ liệu xuất (output) và luồng xữ lý thông qua các ký hiệu hình học.
+// Flowchart của dòng code là:
+// 3)	Nested conditionals là điều kiện có nhiều nhiều hướng, vd như:
+// let loop = true;
+// while (loop) {
+// let a = Number(prompt(`Số bạn đoán là:`));
+// if (a < 10) {
+// console.log(`Thấp rồi!`);
+// } else if (a > 10) {
+// console.log(`Cao rồi!`);
+//     		} else {
+//         		console.log(`BINGO!`);
+//         		break;
+// }
